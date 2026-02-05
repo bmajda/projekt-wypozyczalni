cb33be8d04359859779331b39790fd495db6a855
bmajda <b.majda1017@gmail.com>
2026-02-05 19:02:49 +0100

    Dodanie stylów CSS

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Produkt
 
-# Create your views here.
+def lista_produktow(request):
+    # Pobieramy wszystkie produkty z bazy danych
+    produkty = Produkt.objects.all()
+    
+    # Przekazujemy je do szablonu HTML
+    return render(request, 'core/lista_produktow.html', {'produkty': produkty})
